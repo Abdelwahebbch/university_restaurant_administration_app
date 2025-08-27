f8dad3c5612166b987d8bcd4c99d53bbb6286f60
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-08-28 00:49:07 +0100

    setup the mainfile

--- wahatapp/src/main/java/com/wahat/Main.java
+++ wahatapp/src/main/java/com/wahat/Main.java
@@ -1,7 +1,26 @@
 package com.wahat;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/PrimaryStage.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Student Management System");
+        // primaryStage.setResizable(false);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
 }
