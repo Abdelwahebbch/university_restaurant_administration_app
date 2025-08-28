ba8bb9ce0023fd46737c3895feca1c95a703161b
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-08-28 16:27:37 +0100

    linking all the interfaces

--- wahatapp/src/main/java/com/wahat/controller/PrimaryStageController.java
+++ wahatapp/src/main/java/com/wahat/controller/PrimaryStageController.java
@@ -1,61 +1,73 @@
 package com.wahat.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.wahat.util.SceneSwitcher;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
-public class PrimaryStageController {
+public class PrimaryStageController implements Initializable {
 
-    @FXML
-    private Button LogoutButton;
+  @FXML
+  private Button LogoutButton;
 
-    @FXML
-    private AnchorPane MainScene;
+  @FXML
+  private AnchorPane MainScene;
 
-    @FXML
-    private Button dashboardButton;
+  @FXML
+  private Button dashboardButton;
 
-    @FXML
-    private Button inventoryButton;
+  @FXML
+  private Button inventoryButton;
 
-    @FXML
-    private Button patientsButton;
+  @FXML
+  private Button patientsButton;
 
-    @FXML
-    private Button salesButton;
+  @FXML
+  private Button salesButton;
 
-    @FXML
-    private Button settingsButton;
+  @FXML
+  private Button settingsButton;
 
-    @FXML
-    void handleDashboardButton(ActionEvent event) {
+  @FXML
+  void handleAddStudentButton(ActionEvent event) {
+    SceneSwitcher.setContent(MainScene, "/fxml/Modification/AddStudentScene.fxml");
+  }
 
-    }
+  @FXML
+  void handleDashboardButton(ActionEvent event) {
+    SceneSwitcher.setContent(MainScene, "/fxml/Affichage/Dashboard.fxml");
+  }
 
-    @FXML
-    void handleInventoryButton(ActionEvent event) {
+  @FXML
+  void handleLogoutButton(ActionEvent event) {
 
-    }
+  }
 
-    @FXML
-    void handleLogoutButton(ActionEvent event) {
+  @FXML
+  void handleRechargeButton(ActionEvent event) {
 
-    }
+    SceneSwitcher.setContent(MainScene, "/fxml/Modification/SalesScene.fxml");
+  }
 
-    @FXML
-    void handlePatientsButton(ActionEvent event) {
+  @FXML
+  void handleSettingsButton(ActionEvent event) {
+    SceneSwitcher.setContent(MainScene, "/fxml/Affichage/Settings.fxml");
+  }
 
-    }
+  @FXML
+  void handleStudentListButton(ActionEvent event) {
+    SceneSwitcher.setContent(MainScene, "/fxml/Affichage/StudentsListScene.fxml");
+  }
 
-    @FXML
-    void handleSalesButton(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handleSettingsButton(ActionEvent event) {
-
-    }
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    SceneSwitcher.setContent(MainScene, "/fxml/Affichage/Dashboard.fxml");
+  }
 
 }
